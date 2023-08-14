cebb22e3e312ef12b88cd9009d7634ceaeec9eb6
Ayoub <aero.glowsky@gmail.com>
2023-08-14 22:57:10 +0100

    working version still need betty style

--- exit.c
+++ exit.c
@@ -9,9 +9,9 @@
  * @argv_0: Name of the shell program
  * Return: 0 if the shell should exit, -1 on error
  */
-int exit_builtin(char **tokens) 
-{
-    int exit_status = _atoi(tokens[1]);
+int exit_builtin(char **tokens) {
+    int exit_status;
+
     // Check if the "exit" command was used with the correct number of arguments
     if (tokens[1] == NULL) {
         // No argument, exit with status 0
@@ -22,7 +22,12 @@ int exit_builtin(char **tokens)
         return -1;
     }
 
-    
-    return (exit_status);
+    // Convert the status argument to an integer
+    exit_status = _atoi(tokens[1]);
+
+    // Free allocated memory (tokens, line, line_copy, etc.) if necessary
+
+    // Return the exit status
+    return exit_status;
 }
 
--- shell.c
+++ shell.c
@@ -11,75 +11,83 @@
 
 int main(int ac, char **argv)
 {
-	char *line = NULL, *line_copy = NULL, argv_0[100];
-	char **tokens = NULL;
-	size_t linecap = 0;
-	ssize_t linelen;
-	const char *delim = " \n";
-	int number_of_tokens = 0;
-	char *a_token;
-	int i;
 
-	(void)ac;
-	/* copy of argv[0] to use it when printing errors */
-	_strcpy(argv_0, argv[0]);
-	while (1)
-	{
-		prompt();
-		linelen = getline(&line, &linecap, stdin);
-		if (linelen == -1)
-		{
-			_printf("\n");
-			return (-1);
-		}
+    char *line = NULL, *line_copy = NULL, argv_0[100];
+    char **tokens = NULL;
+    size_t linecap = 0;
+    ssize_t linelen;
+    const char *delim = " \n";
+    int number_of_tokens = 0;
+    char *a_token;
+    int i;
 
-		line_copy = malloc(sizeof(char) * linelen);
-		if (!line_copy)
-		{
-			perror("tsh: memory allocation error");
-			return (-1);
-		}
-		_strcpy(line_copy, line);
-		/* split the string (line) into an array of words */
-		number_of_tokens = 0;
-		a_token = strtok(line, delim);
-		while (a_token != NULL)
-		{
-			number_of_tokens++;
-			a_token = strtok(NULL, delim);
-		}
-		number_of_tokens++;
+    (void)ac;
+    /* copy of argv[0] to use it when printing errors */
+    _strcpy(argv_0, argv[0]);
 
-		tokens = malloc(sizeof(char *) * number_of_tokens);
-		if (!tokens)
-		{
-			perror("tsh: memory allocation error");
-			free(line_copy);
-			return (-1);
-		}
-		
-		a_token = strtok(line_copy, delim);
-		for (i = 0; a_token != NULL; i++)
-		{
-			tokens[i] = malloc(sizeof(char) * (strlen(a_token) + 1));
-			_strcpy(tokens[i], a_token);
-			a_token = strtok(NULL, delim);
-		}
-		tokens[i] = NULL;
 
-		/* Check if the first command is "exit" */
-		if (tokens[0] && strcmp(tokens[0], "exit") == 0)
-		{
-			int exit_status = exit_builtin(tokens);
-			return exit_status;
-		}
-		execmd(tokens, argv_0);
-		/* Free the tokens and line_copy */
-		for (i = 0; tokens[i]; i++)
-			free(tokens[i]);
-	}
-	free(tokens);
-	free(line_copy);
-	free(line);
+    /* ... Your other code ... */
+
+    while (1)
+    {
+        prompt();
+
+        linelen = getline(&line, &linecap, stdin);
+        if (linelen == -1)
+        {
+            _printf("\n");
+            return (-1);
+        }
+
+        line_copy = malloc(sizeof(char) * linelen);
+        if (!line_copy)
+        {
+            perror("tsh: memory allocation error");
+            return (-1);
+        }
+        _strcpy(line_copy, line);
+
+        /* split the string (line) into an array of words */
+        number_of_tokens = 0;
+        a_token = strtok(line, delim);
+        while (a_token != NULL)
+        {
+            number_of_tokens++;
+            a_token = strtok(NULL, delim);
+        }
+        number_of_tokens++;
+
+        tokens = malloc(sizeof(char *) * number_of_tokens);
+        if (!tokens)
+        {
+            perror("tsh: memory allocation error");
+            free(line_copy);
+            return (-1);
+        }
+
+        a_token = strtok(line_copy, delim);
+        for (i = 0; a_token != NULL; i++)
+        {
+            tokens[i] = malloc(sizeof(char) * (strlen(a_token) + 1)); // +1 for '\0'
+            _strcpy(tokens[i], a_token);
+            a_token = strtok(NULL, delim);
+        }
+        tokens[i] = NULL;
+
+        /* Check if the first command is "exit" */
+        if (tokens[0] && strcmp(tokens[0], "exit") == 0) {
+            int exit_status = exit_builtin(tokens);
+            // Free allocated memory (tokens, line, line_copy, etc.) if necessary
+            return exit_status;
+        }
+
+        execmd(tokens, argv_0);
+
+        /* Free the tokens and line_copy */
+        for (i = 0; tokens[i]; i++)
+            free(tokens[i]);
+    }
+    free(tokens);
+    free(line_copy);
+    free(line);
 }
-
