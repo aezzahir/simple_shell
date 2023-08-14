ed09146d77909525999618be45adaec1a64f6748
oussamaNAJIH <oussamanajih6@gmail.com>
2023-08-14 21:36:47 +0200

    env

--- shell.c
+++ shell.c
@@ -18,6 +18,7 @@ int main(int ac, char **argv)
     ssize_t linelen;
     const char *delim = " \n";
     int number_of_tokens = 0;
+    int exit_status;
     char *a_token;
     int i;
 
@@ -76,7 +77,7 @@ int main(int ac, char **argv)
 
         /* Check if the first command is "exit" */
         if (tokens[0] && strcmp(tokens[0], "exit") == 0) {
-            int exit_status = exit_builtin(tokens, argv_0);
+            exit_status = exit_builtin(tokens, argv_0);
             // Free allocated memory (tokens, line, line_copy, etc.) if necessary
             return exit_status;
         }
