c7bbf40baf2caf8788f99549e6ae6c0046c0677c
oussamaNAJIH <oussamanajih6@gmail.com>
2023-08-14 21:58:53 +0200

    retour

--- exit.c
+++ exit.c
@@ -9,8 +9,7 @@
  * @argv_0: Name of the shell program
  * Return: 0 if the shell should exit, -1 on error
  */
-int exit_fucntion(char **tokens, const char *argv_0) {
-    int status;
+int exit_builtin(char **tokens, const char *argv_0) {
     // Check if the "exit" command was used with the correct number of arguments
     if (tokens[1] == NULL) {
         // No argument, exit with status 0
@@ -22,11 +21,11 @@ int exit_fucntion(char **tokens, const char *argv_0) {
     }
 
     // Convert the status argument to an integer
-    status = _atoi(tokens[1]);
+    int exit_status = _atoi(tokens[1]);
 
     // Free allocated memory (tokens, line, line_copy, etc.) if necessary
 
     // Return the exit status
-    return status;
+    return exit_status;
 }
 
