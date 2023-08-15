8cb85ea49f6ea7702e7599483295564231fb2f6e
oussamaNAJIH <oussamanajih6@gmail.com>
2023-08-15 19:47:21 +0200

    set and unset

--- setenv.c
+++ setenv.c
@@ -9,32 +9,30 @@
  */
 int _setenv(char *variable, char *value, int *num_vars_ptr)
 {
-    int i;
-    if (*num_vars_ptr >= MAX_ENV_VARS)
-    {
-        perror("Error: Too many environment variables\n");
-        return -1;
-    }
+int i;
+if (*num_vars_ptr >= MAX_ENV_VARS)
+{
+perror("Error: Too many environment variables\n");
+return (-1);
+}
+for (i = 0; i < *num_vars_ptr; i++)
+{
+if (_strcmp(env_vars[i], variable) == 0)
+{
+_strcpy(env_vals[i], value);
+return (0);
+}
+}
 
-    for (i = 0; i < *num_vars_ptr; i++)
-    {
-        if (_strcmp(env_vars[i], variable) == 0)
-        {
-            _strcpy(env_vals[i], value);
-            return 0;
-        }
-    }
-
-    if (_strlen(variable) >= MAX_VAR_LENGTH || _strlen(value) >= MAX_VALUE_LENGTH)
-    {
-        perror("Error: Variable or value is too long\n");
-        return -1;
-    }
-
-    _strcpy(env_vars[*num_vars_ptr], variable);
-    _strcpy(env_vals[*num_vars_ptr], value);
-    (*num_vars_ptr)++;
-    return 0;
+if (_strlen(variable) >= MAX_VAR_LENGTH || _strlen(value) >= MAX_VALUE_LENGTH)
+{
+perror("Error: Variable or value is too long\n");
+return (-1);
+}
+_strcpy(env_vars[*num_vars_ptr], variable);
+_strcpy(env_vals[*num_vars_ptr], value);
+(*num_vars_ptr)++;
+return (0);
 }
 
 /**
@@ -45,20 +43,20 @@ int _setenv(char *variable, char *value, int *num_vars_ptr)
  */
 int _unsetenv(char *variable, int *num_vars_ptr)
 {
-    int i;
-    for (i = 0; i < *num_vars_ptr; i++)
-    {
-        if (_strcmp(env_vars[i], variable) == 0)
-        {
-            for (int j = i; j < *num_vars_ptr - 1; j++)
-            {
-                _strcpy(env_vars[j], env_vars[j + 1]);
-                _strcpy(env_vals[j], env_vals[j + 1]);
-            }
-            (*num_vars_ptr)--;
-            return 0;
-        }
-    }
-    perror("Variable not found");
-    return -1;
+int i;
+for (i = 0; i < *num_vars_ptr; i++)
+{
+if (_strcmp(env_vars[i], variable) == 0)
+{
+for (int j = i; j < *num_vars_ptr - 1; j++)
+{
+_strcpy(env_vars[j], env_vars[j + 1]);
+_strcpy(env_vals[j], env_vals[j + 1]);
+}
+(*num_vars_ptr)--;
+return (0);
+}
+}
+perror("Variable not found");
+return (-1);
 }
