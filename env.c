80da895c84d3dea9f66fd327f22b4999ee3a4448
oussamaNAJIH <oussamanajih6@gmail.com>
2023-08-15 12:15:44 +0200

    betty env

--- env.c
+++ env.c
@@ -2,17 +2,16 @@
 
 /**
  * print_environment - prints the env variable
- * @environ: the global variable to print
+ * @env: the global variable to print
  */
 
 
 void print_environment(char **env)
 {
-extern char **environ;
 int i = 0;
-while (environ[i] != NULL)
+while (env[i] != NULL)
 {
-_printf(environ[i]);
+_printf(env[i]);
 _printf("\n");
 i++;
 }
