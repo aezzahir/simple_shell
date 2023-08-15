6157ed85cd968db9b5c30a007f200164b6d7d6c6
oussamaNAJIH <oussamanajih6@gmail.com>
2023-08-15 12:35:57 +0200

    betty exit

--- exit.c
+++ exit.c
@@ -1,32 +1,25 @@
 #include "main.h"
 
 
-// ... (Other function declarations, such as _strcpy and execmd)
-
 /**
  * exit_builtin - Implementation of the "exit" built-in command
  * @tokens: Array of tokens from the user input
  * @argv_0: Name of the shell program
  * Return: 0 if the shell should exit, -1 on error
  */
-int exit_builtin(char **tokens) {
-    int exit_status;
-
-    // Check if the "exit" command was used with the correct number of arguments
-    if (tokens[1] == NULL) {
-        // No argument, exit with status 0
-        return 0;
-    } else if (tokens[2] != NULL) {
-        // Too many arguments
-        perror("too many arguments\n");
-        return -1;
-    }
-
-    // Convert the status argument to an integer
-    exit_status = _atoi(tokens[1]);
-
-
-    // Return the exit status
-    return exit_status;
+int exit_builtin(char **tokens)
+{
+int exit_status;
+if (tokens[1] == NULL)
+{
+return (0);
+}
+else if (tokens[2] != NULL)
+{
+perror("too many arguments\n");
+return (-1);
+}
+exit_status = _atoi(tokens[1]);
+return (exit_status);
 }
 
