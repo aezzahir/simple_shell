4d3f86a12ff99460e96b909b93aa4a68131ed763
oussamaNAJIH <oussamanajih6@gmail.com>
2023-08-16 19:26:50 +0200

    handle ;

--- handle_tokens.c
+++ handle_tokens.c
@@ -28,27 +28,40 @@ return (number_of_tokens);
 */
 char **tokenize_input(char *line_copy, const char *delim, int number_of_tokens)
 {
-char **tokens = malloc(sizeof(char *) * number_of_tokens);
-int i;
-char *a_token;
-if (!tokens)
-{
-return (NULL);
+    char **tokens = malloc(sizeof(char *) * number_of_tokens);
+    int i;
+    char *a_token;
+    if (!tokens)
+    {
+        return NULL;
+    }
+    a_token = strtok(line_copy, delim);
+    for (i = 0; a_token != NULL; i++)
+    {
+        if (strcmp(a_token, ";") == 0)
+        {
+            tokens[i] = malloc(sizeof(char) * 2);
+            if (!tokens[i])
+            {
+                memory_allocation_error();
+            }
+            _strcpy(tokens[i], a_token);
+        }
+        else
+        {
+            tokens[i] = malloc(sizeof(char) * (strlen(a_token) + 1));
+            if (!tokens[i])
+            {
+                memory_allocation_error();
+            }
+            _strcpy(tokens[i], a_token);
+        }
+        a_token = strtok(NULL, delim);
+    }
+    tokens[i] = NULL;
+    return tokens;
 }
-a_token = strtok(line_copy, delim);
-for (i = 0; a_token != NULL; i++)
-{
-tokens[i] = malloc(sizeof(char) * (strlen(a_token) + 1));
-if (!tokens[i])
-{
-memory_allocation_error();
-}
-_strcpy(tokens[i], a_token);
-a_token = strtok(NULL, delim);
-}
-tokens[i] = NULL;
-return (tokens);
-}
+
 
 /**
  * memory_allocation_error - return error
