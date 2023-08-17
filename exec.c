a450fdc0f651e13c9cbda26250a5073372d3383a
oussamaNAJIH <oussamanajih6@gmail.com>
2023-08-17 12:37:13 +0200

    handle ;

--- exec.c
+++ exec.c
@@ -58,39 +58,32 @@ print_environment();
  */
 void exec_multiple_cmds(char *line, char *argv_0, const char *delim)
 {
-    int number_of_tokens;
-    char *token;
-    char **tokens;
-    int i;
-
-    const char *d = ";\n";
-    char *line_copy = malloc(sizeof(char) * (strlen(line) + 1));
-    if (!line_copy)
-    {
-        memory_allocation_error();
-        return;
-    }
-    _strcpy(line_copy, line);
-
-    token = strtok(line_copy, d);
-    while (token != NULL)
-    {
-        number_of_tokens = get_number_of_tokens(token, delim);
-        tokens = tokenize_input(token, delim, number_of_tokens);
-        
-        execmd(tokens, argv_0);
-
-        for (i = 0; tokens[i]; i++)
-        {
-            free(tokens[i]);
-        }
-        free(tokens);
-
-        token = strtok(NULL, d);
-    }
-
-    free(line_copy);
+int number_of_tokens, number_of_commands;
+char *token;
+char **tokens;
+int i;
+const char *d = ";";
+char *line_copy = malloc(sizeof(char) * (strlen(line) + 1));
+if (!line_copy)
+{
+memory_allocation_error();
+return;
 }
-
-
+_strcpy(line_copy, line);
+number_of_commands = get_number_of_tokens(line, d);
+token = *tokenize_input(line, d, number_of_commands);
+while (token != NULL)
+{
+tokens = NULL;
+number_of_tokens = get_number_of_tokens(token, delim);
+tokens = tokenize_input(token, delim, number_of_tokens);
+execmd(tokens, argv_0);
+for (i = 0; tokens[i]; i++)
+{
+free(tokens[i]);
+}
+token = strtok(NULL, ";");
+}
+free(line_copy);
+}
 
