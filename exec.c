21c3d97dc5531e4e27fbea877a240d9b64509293
oussamaNAJIH <oussamanajih6@gmail.com>
2023-08-17 13:08:04 +0200

    handle ;

--- exec.c
+++ exec.c
@@ -77,7 +77,7 @@ while (token != NULL)
 tokens = NULL;
 number_of_tokens = get_number_of_tokens(token, delim);
 tokens = tokenize_input(token, delim, number_of_tokens);
-if (strchr(*tokens, ';') == NULL)
+if (strchr(*tokens, ";") == NULL)
 {
 execmd(tokens, argv_0);
 }
