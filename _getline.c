ee6446e2d320052cbbdcc38b5f7fd29ce6266dc3
Ayoub <aero.glowsky@gmail.com>
2023-08-21 20:09:10 +0100

    fix betty getline

--- _getline.c
+++ _getline.c
@@ -1,58 +1,81 @@
 #include "main.h"
 
 /**
- * _getline - get line function
- * @lineptr: command
- * @n: size
- * @stream: file stream
- * Return: number of char
-*/
-
-ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
+ * _realloc_and_copy - Reallocates memory and copies content.
+ * @old_ptr: Pointer to old memory block.
+ * @old_size: Size of the old memory block.
+ * @new_size: Size for the new memory block.
+ * Return: Pointer to the new memory block.
+ */
+static char *_realloc_and_copy(char *old_ptr, size_t old_size, size_t new_size)
 {
-	const size_t initial_size = 128;
-	size_t idx = 0, curr_size;
-	char *ptr, *new_ptr;
-	char c;
-	int fd = fileno(stream);
-	ssize_t bytes_read;
+    char *new_ptr;
 
-	if (!lineptr || !n || fd == -1)
-		return (-1);
-	if (!*lineptr)
-	{
-		*lineptr = malloc(initial_size);
-		if (!*lineptr)
-			return (-1);
-		*n = initial_size;
-	}
-	curr_size = *n;
-	ptr = *lineptr;
-	bytes_read = read(fd, &c, 1);
-	while (bytes_read > 0 && c != '\n')
-	{
-		if (idx >= curr_size - 1)
-		{
-			curr_size *= 2;
-			new_ptr = malloc(curr_size);
-			if (!new_ptr)
-				return (-1);
-			_memcpy(new_ptr, ptr, idx);
-			free(ptr);
-			*n = curr_size;
-			ptr = new_ptr;
-			*lineptr = new_ptr;
-		}
-		ptr[idx++] = c;
-		bytes_read = read(fd, &c, 1);
-	}
-	if (bytes_read > 0 && c == '\n')
-	{
-		ptr[idx++] = c;
-	}
-	ptr[idx] = '\0';
-	if (bytes_read <= 0 && idx == 0)
-		return (-1);
-	return (idx);
+    new_ptr = malloc(new_size);
+    if (!new_ptr)
+        return (NULL);
+    _memcpy(new_ptr, old_ptr, old_size);
+    free(old_ptr);
+    return (new_ptr);
 }
 
+/**
+ * _read_from_fd - Reads a character from a file descriptor.
+ * @fd: File descriptor.
+ * @c: Pointer to char where the read character will be stored.
+ * Return: Number of bytes read.
+ */
+static ssize_t _read_from_fd(int fd, char *c)
+{
+    return (read(fd, c, 1));
+}
+
+/**
+ * _getline - Reads a line from a stream.
+ * @lineptr: Pointer to the buffer that will hold the read line.
+ * @n: Pointer to the size of the buffer.
+ * @stream: Stream to read from.
+ * Return: Number of characters read.
+ */
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
+{
+    const size_t initial_size = 128;
+    size_t idx = 0, curr_size;
+    char c;
+    int fd;
+    ssize_t bytes_read;
+
+    if (!lineptr || !n)
+        return (-1);
+    fd = fileno(stream);
+    if (fd == -1)
+        return (-1);
+    if (!*lineptr)
+    {
+        *lineptr = malloc(initial_size);
+        if (!*lineptr)
+            return (-1);
+        *n = initial_size;
+    }
+    curr_size = *n;
+    bytes_read = _read_from_fd(fd, &c);
+    while (bytes_read > 0 && c != '\n')
+    {
+        if (idx >= curr_size - 1)
+        {
+            curr_size *= 2;
+            *lineptr = _realloc_and_copy(*lineptr, idx, curr_size);
+            if (!*lineptr)
+                return (-1);
+            *n = curr_size;
+        }
+        (*lineptr)[idx++] = c;
+        bytes_read = _read_from_fd(fd, &c);
+    }
+    if (bytes_read > 0 && c == '\n')
+        (*lineptr)[idx++] = c;
+    (*lineptr)[idx] = '\0';
+    if (bytes_read <= 0 && idx == 0)
+        return (-1);
+    return (idx);
+}
