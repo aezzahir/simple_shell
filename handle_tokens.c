e445b1d9272e2ae603cbd865895479862ca24bf8
ayb1337 <aero.glowsky@gmail.com>
2023-08-23 17:43:04 -0700

    ayoub

--- handle_tokens.c
+++ handle_tokens.c
@@ -5,19 +5,19 @@
  * @command: the command line
  * @delim: the delimiter “space” or “:”
  * Return: the number of tokens
-*/
+ */
 int get_number_of_tokens(char *command, const char *delim)
 {
-int number_of_tokens = 0;
-char *a_token;
-a_token = _strtok(command, delim);
-while (a_token != NULL)
-{
-number_of_tokens++;
-a_token = _strtok(NULL, delim);
-}
-number_of_tokens++;
-return (number_of_tokens);
+	int number_of_tokens = 0;
+	char *a_token;
+	a_token = _strtok(command, delim);
+	while (a_token != NULL)
+	{
+		number_of_tokens++;
+		a_token = _strtok(NULL, delim);
+	}
+	number_of_tokens++;
+	return (number_of_tokens);
 }
 /**
  * tokenize_input - function that tokenize a command
@@ -25,38 +25,38 @@ return (number_of_tokens);
  * @delim: the delimiter
  * @number_of_tokens: the number of tokens
  * Return: a pointer to pointer to the tokens’ vector
-*/
+ */
 char **tokenize_input(char *line_copy, const char *delim, int number_of_tokens)
 {
-char **tokens = malloc(sizeof(char *) * number_of_tokens);
-int i;
-char *a_token;
-if (!tokens)
-{
-return (NULL);
-}
-a_token = _strtok(line_copy, delim);
-for (i = 0; a_token != NULL; i++)
-{
-tokens[i] = malloc(sizeof(char) * (strlen(a_token) + 1));
-if (!tokens[i])
-{
-memory_allocation_error();
-}
-_strcpy(tokens[i], a_token);
-a_token = _strtok(NULL, delim);
-}
-tokens[i] = NULL;
-return (tokens);
+	char **tokens = malloc(sizeof(char *) * number_of_tokens);
+	int i;
+	char *a_token;
+	if (!tokens)
+	{
+		return (NULL);
+	}
+	a_token = _strtok(line_copy, delim);
+	for (i = 0; a_token != NULL; i++)
+	{
+		tokens[i] = malloc(sizeof(char) * (strlen(a_token) + 1));
+		if (!tokens[i])
+		{
+			memory_allocation_error();
+		}
+		_strcpy(tokens[i], a_token);
+		a_token = _strtok(NULL, delim);
+	}
+	tokens[i] = NULL;
+	return (tokens);
 }
 
 /**
  * memory_allocation_error - return error
  * Return: -1
-*/
+ */
 
 int memory_allocation_error(void)
 {
-perror("tsh: memory allocation error");
-return (-1);
+	perror("tsh: memory allocation error");
+	return (-1);
 }
